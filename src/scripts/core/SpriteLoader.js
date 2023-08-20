9b492e54c412d68a45d29e3400e4744eefc58863
Ibrahim Ben <63601488+Ibrahim-Benkhedda@users.noreply.github.com>
2023-08-20 02:31:35 +0100

    added SpriteLoader
    Added A class for loading static and animated sprites

---
+++ src/scripts/core/SpriteLoader.js
@@ -0,0 +1,64 @@
+import * as PIXI from 'pixi.js';
+/**
+ * Class for loading static and animated sprites resources
+ */
+export class SpriteLoader {
+    constructor() {
+        // cache for loaded resources, prevents redundant loading
+        this.resources = {};
+    }
+
+    /**
+     * loads a texture from the path or returns the texture
+     * from cache (resources) if it has been loaded before.
+     * @param {string} texturePath - path for the texture file
+     * @returns {PIXI.Texture} - loaded texture
+     */
+    loadTexture(texturePath) {
+        if (this.resources[texturePath]) {
+            return this.resources[texturePath]; // return cached resource if available
+        }
+
+        // loads the texture and cache the loaded texture
+        const texture = PIXI.Texture.from(texturePath);
+        this.resources[texturePath] = texture;
+
+        return texture;
+    }
+
+    /**
+     * loads a static sprite from the texture path
+     * @param {string} texturePath - path for the texture file 
+     * @returns {PIXI.Sprite} - loaded static sprite 
+     */
+    loadStaticSprite(texturePath) {
+        const texture = this.loadTexture(texturePath);
+        return new PIXI.Sprite(texture);
+    }
+
+    /**
+     * loads an animated sprite from the JSON spritesheet data and texture path
+     * @param {Object} jsonData - JSON object that contains the sprite frames
+     * @param {string} texturePath - path for the texture file
+     * @returns {PIXI.AnimatedSprite} - loaded animated sprite
+     */
+    loadAnimatedSprite(jsonData, texturePath) {
+        const frames = [];
+        const texture = this.loadTexture(texturePath);
+
+        // Create texture for each frames in the json object
+        for (let frameData of Object.values(jsonData.frames)) {
+            // get the properties from the frame data
+            const { x, y, w, h } = frameData.frame;
+            // create a new texture with the given dimensions
+            const frame = new PIXI.Texture(texture.baseTexture, new PIXI.Rectangle(x, y, w, h));
+            // add the created texture to the frames array
+            frames.push(frame);
+        }
+
+        // create the animated sprite using the frames array and return it 
+        return new PIXI.AnimatedSprite(frames);
+    }
+
+    
+}
